15bb917bd04d1631bfd078454cd919ff4fc0635f
Rossini Santos <rossinisantos@gmail.com>
2026-02-05 14:47:03 -0300

    feat: adicionar soft delete ao sistema
    - Adicionar coluna deleted_at nas tabelas users, roles e permissions
    - Criar modelo User com trait SoftDeletes
    - Criar modelos Role e Permission extendendo do Spatie
    - Configurar para usar modelos customizados no permission.php
    - Implementar exclusão reversível (dados não são perdidos)
    - Adicionar métodos de restore e forceDelete
    - Testado e funcionando corretamente

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasRoles;
+    use HasFactory, Notifiable, HasRoles, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -43,6 +44,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'deleted_at' => 'datetime',
         ];
     }
 }
